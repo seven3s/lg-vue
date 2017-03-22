05d4193a281efcaa68350d3e362c93d4e80a86cc
花夏 <liubiao0810@live.cn>
2017-03-22 17:42:59 +0800

    更新loader

--- src/demo/index.vue
+++ src/demo/index.vue
@@ -18,25 +18,11 @@
         mounted: function () {},
         methods: {
             testAjax: function () {
-                let url = 'http://api.36wu.com/Weather/GetWeather?district=%E5%8C%97%E4%BA%AC&authkey=7dce6cdbe90c461f8dc807c8399640c5';
+                let url = '/a/a';
                 restFullLoader.requestGET(url)
                 .then((res) => {
                     console.log(res);
                 });
-                // $.ajax({
-                //     url: url,
-                //     type: 'get',
-                //     dataType: 'json'
-                // })
-                // .done(function (res) {
-                //     console.log('success');
-                // })
-                // .fail(function (res) {
-                //     console.log(res);
-                // })
-                // .always(function (res) {
-                //     console.log(res);
-                // });
             }
         }
     };
